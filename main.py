3b0b2c2c490a4bc6c17e2e372eab4b1c05005f55
Bhumika Rawate <bhumikarawate19@gmail.com>
2026-03-23 15:44:23 +0530

    environment setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env", override=True)
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
